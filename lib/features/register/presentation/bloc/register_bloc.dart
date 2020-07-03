9a60ffe1e94563d66e9f330f792e7803ac533a72
Jhamere Wilson <jhamerewilson@MacBook-Pro-2.local>
2020-07-02 22:59:09 -0400

    added features folder

---
+++ lib/features/register/presentation/bloc/register_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+part 'register_event.dart';
+part 'register_state.dart';
+class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
+  @override
+  RegisterState get initialState => RegisterInitial();
+  @override
+  Stream<RegisterState> mapEventToState(
+    RegisterEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
