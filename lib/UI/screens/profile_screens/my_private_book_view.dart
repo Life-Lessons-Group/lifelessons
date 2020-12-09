2f2bc3cbdf473a20756193d1ea77cadaa81bee3b
lake <tousgood@gmail.com>
2020-12-08 22:04:05 -0500

    added text to private book prompt, on profile page

--- lib/UI/screens/profile_screens/my_private_book_view.dart
+++ lib/UI/screens/profile_screens/my_private_book_view.dart
@@ -8,7 +8,7 @@ import 'package:firebase_flutter_life/Models/private_post_model.dart';
 import 'package:flutter/material.dart';
 
 class MyPrivateBookScreen extends StatefulWidget {
- final User currentUser;
+  final User currentUser;
 
   const MyPrivateBookScreen({Key key, this.currentUser}) : super(key: key);
 
@@ -17,16 +17,15 @@ class MyPrivateBookScreen extends StatefulWidget {
 }
 
 class _MyPrivateBookScreenState extends State<MyPrivateBookScreen> {
+  List<PrivatePost> posts;
 
-   List<PrivatePost> posts;
-
-     @override
+  @override
   void initState() {
     super.initState();
     getTimeline();
   }
 
-    getTimeline() async {
+  getTimeline() async {
     final FirebaseUser user = await FirebaseAuth.instance.currentUser();
     QuerySnapshot snapshot = await PostRepository()
         .privatePostsRef
@@ -47,31 +46,57 @@ class _MyPrivateBookScreenState extends State<MyPrivateBookScreen> {
       );
     } else if (posts.isEmpty) {
       return SingleChildScrollView(
-          child: Column(
-        children: <Widget>[
-          SizedBox(height: 70),
-          Icon(Icons.filter_none, color: Colors.black26,),
-              SizedBox(height: 20),
-          Text("It looks like you haven't started your private book yet.", style: TextStyle(color: Colors.black26, fontSize: 16.0),),
-              SizedBox(height: 20),
-          Text("To start a private book, record a lesson and select the option 'add to private book' ", style: TextStyle(color: Colors.black26, fontSize: 16.0), textAlign: TextAlign.center,),
-              SizedBox(height: 30),
-          // SizedBox(
-          //   height: 50,
-          //   width: 250,
-          //           child: FlatButton(
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(20),
-          //           ),
-          //     color: Colors.indigo[300],
-          //     onPressed: (){ Navigator.pushReplacementNamed(context, '/record-screen');},
-          //     child: Text("Record your first private lesson!", style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
-          //   ),
-          // ),
-        
-        ],
-      ),
-    );
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 70),
+            Icon(
+              Icons.filter_none,
+              color: Colors.black26,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Family Recipes? ",
+              style: TextStyle(color: Colors.black, fontSize: 16.0),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "Family Traditions? ",
+              style: TextStyle(color: Colors.black, fontSize: 16.0),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+            Text(
+              "It looks like you haven't started your private book yet.",
+              style: TextStyle(color: Colors.black26, fontSize: 16.0),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "To start a private book, record a lesson and select the option 'add to private book' ",
+              style: TextStyle(color: Colors.black26, fontSize: 16.0),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30),
+            Text(
+              "ACCESS TO YOUR PRIVATE BOOK IS BY INVITATION ONLY",
+              style: TextStyle(color: Colors.red, fontSize: 16.0),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 35),
+            // SizedBox(
+            //   height: 50,
+            //   width: 250,
+            //           child: FlatButton(
+            //           shape: RoundedRectangleBorder(
+            //             borderRadius: BorderRadius.circular(20),
+            //           ),
+            //     color: Colors.indigo[300],
+            //     onPressed: (){ Navigator.pushReplacementNamed(context, '/record-screen');},
+            //     child: Text("Record your first private lesson!", style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
+            //   ),
+            // ),
+          ],
+        ),
+      );
     } else {
       return ListView(children: posts);
     }
